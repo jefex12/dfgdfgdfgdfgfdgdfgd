cb036b134ea5f11650a208583eee41bf1dce1228
jefex12 <38332865+jefex12@users.noreply.github.com>
2018-04-14 19:29:53 +0300

    dasd
    sadasd

--- vpn.sh
+++ vpn.sh
@@ -264,9 +264,5 @@ key-direction 1
 verb 3" > /etc/openvpn/client-common.txt
 	# Generates the custom client.ovpn
 	newclient "$CLIENT"
-	echo ""
-	echo "Finished!"
-	echo ""
-	echo "Your client configuration is available at" ~/"$CLIENT.ovpn"
-	echo "If you want to add more clients, you simply need to run this script again!"
 fi
+	echo "Готово"
